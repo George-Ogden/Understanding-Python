10d616cca6e6069b139c7ef2c4a63d7ed87f7929
George-Ogden <G02287@outlook.com>
2020-08-07 20:11:22 +0100

    Lesson 07 - MINOR UPDATE

--- Lesson 07 - Important Items.py
+++ Lesson 07 - Important Items.py
@@ -2,4 +2,4 @@ importantItems = [7,3.14159,True,"Python",print]
 importantItems[0] = 2357
 importantItems[-1] = len
 print("My important items are:",*importantItems,sep="\n")
-print("There are",len(importantItems),"items that are important.")
+print("There are",len(importantItems),"items that are important in this list.")
