55d44312d77b023b63690679eebfe886f8a74117
George Ogden <George-Ogden@outlook.com>
2020-10-30 09:26:18 +0000

    Lesson 25

---
+++ Lesson 25 - Making Exceptions.py
@@ -0,0 +1,19 @@
+import logging
+
+class OutOfBoundsError(ValueError):
+    pass
+
+level = int(input("What level should we log?\n"))
+if level < 0:
+    raise OutOfBoundsError("Level is too low")
+elif level > 60:
+    raise OutOfBoundsError("Level is too high")
+logging.basicConfig(level=level)
+logging.debug("This is a debug message")
+logging.info("This is a info message")
+logging.warning("This is a warning message")
+logging.error("This is a error message")
+logging.critical("This is a critical message")
+# exit(0)
+quit(0)
+logging.critical("Something has gone wrong")
