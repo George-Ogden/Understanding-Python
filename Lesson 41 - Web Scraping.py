73e14f249c596a38e3a69f3e3bbcbeb3c4a707f5
George Ogden <George-Ogden@outlook.com>
2021-03-14 15:24:18 +0000

    Lesson 41 - MINOR UPDATE

--- Lesson 41 - Web Scraping.py
+++ Lesson 41 - Web Scraping.py
@@ -17,21 +17,21 @@ visited = set()
 while len(to_visit) > 0:
     location = next(iter(to_visit))
     print(location)
-    result = requests.get(location)
-    if "goprogram.co.uk" in result.url:
-        soup = BeautifulSoup(result.content,features="lxml")
-        for link in soup.find_all("a"):
-            href = link.get("href")
-            if href == None:
-                continue
-            href = href.split("#")[0]
-            if href.startswith("/"):
-                href = "https://www.goprogram.co.uk" + href
-            elif not href.startswith("https://"):
-                href = "/".join(location.split("/")[:-1]) + "/" + href
-            if not href in visited:
-                to_visit.update([href])
-        valid.update([location])
-    visited.update([location])
-    to_visit.discard(location)
+    with requests.get(location) as result:
+        if "goprogram.co.uk" in result.url:
+            soup = BeautifulSoup(result.content,features="lxml")
+            for link in soup.find_all("a"):
+                href = link.get("href")
+                if href == None:
+                    continue
+                href = href.split("#")[0]
+                if href.startswith("/"):
+                    href = "https://www.goprogram.co.uk" + href
+                elif not href.startswith("https://"):
+                    href = "/".join(location.split("/")[:-1]) + "/" + href
+                if not href in visited:
+                    to_visit.update([href])
+            valid.update([location])
+        visited.update([location])
+        to_visit.discard(location)
 print(valid)
