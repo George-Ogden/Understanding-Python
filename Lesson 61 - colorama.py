3940ef803359de723509926204c0cc95687bba1e
George Ogden <George-Ogden@outlook.com>
2021-08-15 11:47:56 +0100

    Lesson 61

---
+++ Lesson 61 - colorama.py
@@ -0,0 +1,18 @@
+from colorama import Fore, Back, Style, init
+import argparse
+parser = argparse.ArgumentParser("colorama demo")
+
+def format(module):
+    return list(map(lambda x: x.lower(),module.__dict__.keys()))
+
+def find(module,item):
+    return module.__dict__[item.upper()]
+
+parser.add_argument("-c","--colour",choices=format(Fore),default="RESET")
+parser.add_argument("-b","--background",choices=format(Back),default="RESET")
+parser.add_argument("-s","--style",choices=format(Style),default="RESET_ALL")
+parser.add_argument("-t","--text",default="Lorem ipsum dolor sit amet")
+
+args = parser.parse_args()
+
+print(find(Style,args.style) + find(Fore,args.colour) + find(Back,args.background) + args.text + Style.RESET_ALL)
