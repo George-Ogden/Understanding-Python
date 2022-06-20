63ab7e22053c7b8af20af4927e9a0ca8273366a8
George Ogden <George-Ogden@outlook.com>
2022-06-20 07:43:23 +0100

    Lesson 95 - MINOR UPDATE

--- Lesson 95 - MIDIs/main.py
+++ Lesson 95 - MIDIs/main.py
@@ -7,17 +7,19 @@ import numpy as np
 sample_rate = 44100
 
 multitrack = read("fur-elise.mid")
-multitrack.trim(0, 12 * multitrack.resolution)
+# multitrack.trim(0, 12 * multitrack.resolution)
 print(multitrack)
 multitrack.plot()
 plt.show()
 
+multitrack[0].program = 29
+multitrack[1].program = 87
 drum_beat = np.zeros((multitrack.get_max_length(), 128), dtype=np.uint8)
-drum_beat[multitrack.downbeat[:,0], 70] = 30
+drum_beat[multitrack.downbeat[:,0], 40] = 50
 drum_track = StandardTrack("drum", is_drum=True, pianoroll=drum_beat, program=0)
 multitrack.append(drum_track)
 
-wave = to_pretty_midi(multitrack).fluidsynth(sf2_path="soundfont.sf2", fs=sample_rate)
+wave = to_pretty_midi(multitrack).fluidsynth(fs=sample_rate)
 plt.plot(wave)
 plt.show()
 
